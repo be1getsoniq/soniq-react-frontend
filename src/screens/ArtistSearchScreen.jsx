0935fa47acf9bf8d550a31d97d9c9130ef970d3d
Neha Chauhan <be1@getsoniq.app>
2025-08-30 20:03:19 +0530

    updated userid accept

--- src/screens/ArtistSearchScreen.jsx
+++ src/screens/ArtistSearchScreen.jsx
@@ -13,7 +13,12 @@ export default function ArtistSearch() {
   const [apiResponse, setApiResponse] = useState([]);
   const navigate = useNavigate();
 
-  const userId = "cmecddj2x0000h5o7dx069be6";
+  const { search } = useLocation();
+
+  const params = new URLSearchParams(search);
+  const userId = params.get("userId");
+
+  // const userId = "cmecddj2x0000h5o7dx069be6";
   let BASE_URL = getBaseUrl();
 
   // const [followed, setFollowed] = useState(false);
@@ -86,11 +91,10 @@ export default function ArtistSearch() {
 
   const handleSearch = async () => {
     if (!query) return;
-    
+
     console.log(`${BASE_URL}/api/${source}/artists/albums`);
     console.log(`${BASE_URL}/api/auth/token/${userId}`);
-    
-    
+
     try {
       const token = await getToken();
 
@@ -135,58 +139,60 @@ export default function ArtistSearch() {
   return (
     <div className="max-w-xl mx-auto p-6 space-y-6 relative">
       <h1 className="text-2xl text-gray-100 font-bold">Artist Search</h1>
-      <div>
-        <div className="flex gap-2 mb-4">
-          <button
-            className={`px-4 py-2 rounded transition ${
-              source === "spotify"
-                ? "bg-green-600 text-white"
-                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-            }`}
-            onClick={() => setSource("spotify")}
-          >
-            Spotify
-          </button>
+      {userId ? (
+        <div id="loggedIn">
+          <div>
+            <div className="flex gap-2 mb-4">
+              <button
+                className={`px-4 py-2 rounded transition ${
+                  source === "spotify"
+                    ? "bg-green-600 text-white"
+                    : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+                }`}
+                onClick={() => setSource("spotify")}
+              >
+                Spotify
+              </button>
 
-          <button
-            className={`px-4 py-2 rounded transition ${
-              source === "apple"
-                ? "bg-green-600 text-white"
-                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-            }`}
-            onClick={() => setSource("apple")}
-          >
-            Apple
-          </button>
-        </div>
-          <p className="text-white font-semibold pb-3">{source} Search</p>
-        <input
-          value={query}
-          onChange={(e) => {
-            const value = e.target.value;
-            setQuery(value);
+              <button
+                className={`px-4 py-2 rounded transition ${
+                  source === "apple"
+                    ? "bg-green-600 text-white"
+                    : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+                }`}
+                onClick={() => setSource("apple")}
+              >
+                Apple
+              </button>
+            </div>
+            <p className="text-white font-semibold pb-3">{source} Search</p>
+            <input
+              value={query}
+              onChange={(e) => {
+                const value = e.target.value;
+                setQuery(value);
 
-            clearTimeout(window.searchTimeout);
-            window.searchTimeout = setTimeout(() => {
-              if (value.trim() !== "") {
-                handleSearch(value);
-              }
-            }, 2600);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && query.trim() !== "") {
-              clearTimeout(window.searchTimeout);
-              handleSearch(query);
-            }
-          }}
-          onBlur={() => {
-            if (query.trim() !== "") {
-              clearTimeout(window.searchTimeout);
-              handleSearch(query);
-            }
-          }}
-          placeholder="Enter artist name"
-          className="
+                clearTimeout(window.searchTimeout);
+                window.searchTimeout = setTimeout(() => {
+                  if (value.trim() !== "") {
+                    handleSearch(value);
+                  }
+                }, 2600);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && query.trim() !== "") {
+                  clearTimeout(window.searchTimeout);
+                  handleSearch(query);
+                }
+              }}
+              onBlur={() => {
+                if (query.trim() !== "") {
+                  clearTimeout(window.searchTimeout);
+                  handleSearch(query);
+                }
+              }}
+              placeholder="Enter artist name"
+              className="
               w-full px-4 py-2 rounded-xl
               bg-gray-700
               placeholder-gray-400
@@ -196,55 +202,63 @@ export default function ArtistSearch() {
               focus:ring-gray-500
               shadow-inner
             "
-        />
-      </div>
-      <div className="grid grid-cols-1 gap-4">
-        {results.map((artist) => (
-          <div
-            key={artist.id}
-            className="flex items-center justify-between bg-gray-800
-      rounded-xl border border-gray-700 shadow p-4"
-          >
-            <a key={artist.id} href={artist.url} target="_blank">
-              <div className="flex items-center gap-4">
-                {artist.artwork && (
-                  <img
-                    src={artist.artwork}
-                    alt={artist.name}
-                    className="w-12 h-12 rounded-full object-cover"
-                  />
-                )}
-
-                <div>
-                  <p className="text-gray-100 font-semibold">{artist.name}</p>
-                  <p className="text-sm text-gray-400">
-                    {artist.genreNames[0]}
-                  </p>
-                </div>
-              </div>
-            </a>
-
-            <button
-              className="px-4 py-1 bg-green-600 cursor-pointer text-white rounded-full text-sm"
-              onClick={() => handleFollow(artist.id, artist.name)}
-            >
-              {followedIds.some((a) => a.id === artist.id) ? (
-                <UserCheck size={18} />
-              ) : (
-                "Follow"
-              )}
-            </button>
+            />
           </div>
-        ))}
-      </div>
+          <div className="grid grid-cols-1 gap-4">
+            {results.map((artist) => (
+              <div
+                key={artist.id}
+                className="flex items-center justify-between bg-gray-800
+      rounded-xl border border-gray-700 shadow p-4"
+              >
+                <a key={artist.id} href={artist.url} target="_blank">
+                  <div className="flex items-center gap-4">
+                    {artist.artwork && (
+                      <img
+                        src={artist.artwork}
+                        alt={artist.name}
+                        className="w-12 h-12 rounded-full object-cover"
+                      />
+                    )}
 
-      <button
-        className="fixed bottom-4 left-1/2 -translate-x-1/2 px-8 py-3 bg-green-600 
+                    <div>
+                      <p className="text-gray-100 font-semibold">
+                        {artist.name}
+                      </p>
+                      <p className="text-sm text-gray-400">
+                        {artist.genreNames[0]}
+                      </p>
+                    </div>
+                  </div>
+                </a>
+
+                <button
+                  className="px-4 py-1 bg-green-600 cursor-pointer text-white rounded-full text-sm"
+                  onClick={() => handleFollow(artist.id, artist.name)}
+                >
+                  {followedIds.some((a) => a.id === artist.id) ? (
+                    <UserCheck size={18} />
+                  ) : (
+                    "Follow"
+                  )}
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <button
+            className="fixed bottom-4 left-1/2 -translate-x-1/2 px-8 py-3 bg-green-600 
                      text-white rounded-full shadow-lg cursor-pointer"
-        onClick={handleContinue}
-      >
-        Continue
-      </button>
+            onClick={handleContinue}
+          >
+            Continue
+          </button>
+        </div>
+      ) : (
+        <div id="notloggedIn">
+          <p>Please Login</p>
+        </div>
+      )}
     </div>
   );
 }
